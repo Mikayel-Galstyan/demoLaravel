735fc534c114f371e17bfcf29219e6e2b7caeb97
miqololo <miko.galstyan@gmail.com>
2017-07-17 15:50:38 +0500

    Add some validation changes

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -49,8 +49,8 @@ class RegisterController extends Controller
     protected function validator(array $data)
     {
         return Validator::make($data, [
-            'first_name' => 'required|string|max:255',
-            'last_name' => 'required|string|max:255',
+            'first_name' => 'required|string|max:120',
+            'last_name' => 'required|string|max:120',
             'username' => 'required|min:4|max:255|unique:users',
             'password' => 'required|string|min:6|confirmed',
         ]);
